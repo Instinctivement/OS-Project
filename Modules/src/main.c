dfd4c8efa30ff33a462396ed427e26bd6f4ab856
Stanley <stansky666@gmail.com>
2023-06-04 02:46:13 +0200

    V2

--- Modules/src/main.c
+++ Modules/src/main.c
@@ -1,12 +1,12 @@
 #include "includes.h"
 
-const char *fichierProd = "../Production/ListeFichiers.txt";
-const char *fichierBack = "../Backup/ListeFichiers.txt";
-const char *fichierInt = "src/IntergrationServer/ListeFichiers.txt";
-char *nom_fichier_copie = "src/listToCopy.txt";
+const char *myProdFile = "../Production/ListeFichiers.txt";
+const char *myBackFile = "../Backup/ListeFichiers.txt";
+const char *myIntFile = "src/IntergrationServer/ListeFichiers.txt";
+char *copyFile = "src/listToCopy.txt";
 
-void copy_list_thread();
-void synchro_list_thread();
+void copy_list_main();
+void synchro_list_main();
 
 int main() {
 
@@ -21,9 +21,9 @@ int main() {
         testServerB();
 
         printf("\n\n------------------ Synchronisations Des Serveurs ----------------------\n\n");
-        synchro_list_thread();
+        synchro_list_main();
 
-        copy_list_thread();
+        copy_list_main();
 
         printf("\n\tSTATISTIQUES\n");
         printf("\t----------------------------------------\n");
@@ -38,34 +38,34 @@ int main() {
 }
 
 
-void synchro_list_thread() {
-    synchroList(fichierProd, fichierInt);
-    synchroList(fichierProd, fichierBack);
+void synchro_list_main() {
+    synchroList(myProdFile, myIntFile);
+    synchroList(myProdFile, myBackFile);
 }
 
-void copy_list_thread() {
+void copy_list_main() {
 
     // Ouverture de la liste des fichiers du serveur de backup
-    int fichier_liste_backup = open(fichierBack, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    int fichier_liste_backup = open(myBackFile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (fichier_liste_backup == -1) {
         perror("filesProd : Erreur d'ouverture de la liste des fichiers du serveur de backup");
         exit(EXIT_FAILURE);
     }
 
     printf("\n\n------------------ Copie Vers Le Serveurs De Backup ----------------------\n\n");
-    copy_list(nom_fichier_copie, "../Production/home", "../Backup/home");
+    copy_list(copyFile, "../Production/home", "../Backup/home");
     generer_liste_fichiers("../Backup/home/", fichier_liste_backup);
     save_data_log(fichier_log, "Module copy: Copy de list réussi vers Backup");
 
     // Ouverture de la liste des fichiers du serveur de integration
-    int fichier_liste_int = open(fichierInt, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    int fichier_liste_int = open(myIntFile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (fichier_liste_int == -1) {
         perror("filesProd : Erreur d'ouverture de la liste des fichiers du serveur de integration");
         exit(EXIT_FAILURE);
     }
 
     printf("\n\n------------------ Copie Vers Le Serveurs D'Intégration ----------------------\n\n");
-    copy_list(nom_fichier_copie, "../Production/home", "src/IntergrationServer/home");
+    copy_list(copyFile, "../Production/home", "src/IntergrationServer/home");
     generer_liste_fichiers("src/IntergrationServer/home/", fichier_liste_int);
     save_data_log(fichier_log, "Module copy: Copy de list réussi vers Integration");
 }
--- Modules/src/tools.c
+++ Modules/src/tools.c
@@ -5,9 +5,9 @@
 #include "tools.h"
 #define MAX_LINE_LENGTH 512
 
-char* fichier_log = "src/ModLog/log.txt";
-char* fichier_stat_erreur = "src/ModStat/stat_erreur.txt";
-char* fichier_stat_fichier_recu = "src/ModStat/stat_fichier_recu.txt";
+char *fichier_log = "src/ModLog/log.txt";
+char *fichier_stat_erreur = "src/ModStat/stat_erreur.txt";
+char *fichier_stat_fichier_recu = "src/ModStat/stat_fichier_recu.txt";
 
 int save_data_log(const char *file_log, const char *data)
 {
@@ -20,7 +20,7 @@ int save_data_log(const char *file_log, const char *data)
         printf("Erreur lors de l'ouverture de fichier\n.");
         save_error("Erreur lors de l'ouverture de fichier");
         return EXIT_FAILURE;
-    } 
+    }
     else
     {
         fprintf(fichier, "%s\t", data);
@@ -41,11 +41,13 @@ int get_error_count()
     return file_line_count(fichier_stat_erreur);
 }
 
-int compare_files(const char* file1, const char* file2) {
-    FILE* f1 = fopen(file1, "r");
-    FILE* f2 = fopen(file2, "r");
+int compare_files(const char *file1, const char *file2)
+{
+    FILE *f1 = fopen(file1, "r");
+    FILE *f2 = fopen(file2, "r");
 
-    if (f1 == NULL || f2 == NULL) {
+    if (f1 == NULL || f2 == NULL)
+    {
         printf("Erreur lors de l'ouverture des fichiers.\n");
         return 0;
     }
@@ -53,14 +55,17 @@ int compare_files(const char* file1, const char* file2) {
     int c1, c2;
     int different = 0;
 
-    while ((c1 = fgetc(f1)) != EOF && (c2 = fgetc(f2)) != EOF) {
-        if (c1 != c2) {
+    while ((c1 = fgetc(f1)) != EOF && (c2 = fgetc(f2)) != EOF)
+    {
+        if (c1 != c2)
+        {
             different = 1;
             break;
         }
     }
 
-    if (c1 != c2) {
+    if (c1 != c2)
+    {
         different = 1;
     }
 
@@ -75,7 +80,7 @@ int get_received_file_count()
     return file_line_count(fichier_stat_fichier_recu);
 }
 
-int file_line_count(char* namefile)
+int file_line_count(char *namefile)
 {
 
     FILE *fichier = fopen(namefile, "r");
@@ -102,19 +107,18 @@ int file_line_count(char* namefile)
     return nombreLignes;
 }
 
-int save_error(char* description)
+int save_error(char *description)
 {
     return write_end_of_file_stat_line(description, fichier_stat_erreur);
 }
 
-int save_received_file(char* namefile)
+int save_received_file(char *namefile)
 {
     return write_end_of_file_stat_line(namefile, fichier_stat_fichier_recu);
 }
 
-int write_end_of_file_stat_line(char* line, char* namefile)
+int write_end_of_file_stat_line(char *line, char *namefile)
 {
-
     FILE *f = NULL;
     time_t now;
     struct tm *local_time;
@@ -129,7 +133,7 @@ int write_end_of_file_stat_line(char* line, char* namefile)
 
     if (f == NULL)
     {
-        printf("Erreur lors de l'ouverture du fichier, pour l'ecriture en fin de fichier");
+        printf("Erreur lors de l'ouverture du fichier.");
         return 1;
     }
 
